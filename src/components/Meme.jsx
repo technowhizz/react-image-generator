8cc45f6a43c6b4ad9405f0e5c56875be7c0aded2
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-08 02:34:49 +0100

    Add image with working React states

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import MemesData from "../MemesData"
 
 export default function Meme(){
+
+
+    const [allMemeImages, setAllMemeImages] = React.useState(MemesData)
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        image: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(oldValues => ({
+            ...oldValues,
+            image: url
+        }))
+
+    }
+
+
     return(
         <main className="meme">
             <div className="meme--form">
@@ -16,7 +39,10 @@ export default function Meme(){
                         placeholder="Bottom Text" >
                     </input>
                 </div>
-                <button className="meme--form-button">Get a new meme image</button>
+                <button onClick={getMemeImage} className="meme--form-button">
+                    Get a new meme image 🖼
+                </button>
+                <img className="meme--image" src={meme.image} />
             </div>
         </main>
     )
